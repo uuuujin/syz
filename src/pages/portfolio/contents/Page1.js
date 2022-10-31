a14f3c9b019b11b0f983afedced6f7d7fc00365c
yjshin2 <yjshin2@enliple.com>
2022-10-31 18:43:46 +0900

    push test

--- src/pages/portfolio/contents/Page1.js
+++ src/pages/portfolio/contents/Page1.js
@@ -1,9 +1,25 @@
 import React from "react";
 import './Page.scss';
 //import { useState } from "react";
+import {useEffect} from "react";
 //import ReactDOM from "react-dom";
 
 export default function Page1() {
+
+    const ResizedComponent = () => {
+        const handleResize = () => {
+            console.log(`브라우저 화면 사이즈 : ${window.innerWidth}, y : ${window.innerHeight}`);
+        }
+
+         useEffect(( ) => {
+            window.addEventListener('resize', handleResize);
+            return () => {  //cleanup
+                window.removeEventListener('resize', handleResize);
+            }
+        }, []);
+    }
+
+
     //const [resize, setResize] = useState();
 
     const w = window.innerWidth + 'px';
@@ -17,6 +33,12 @@ export default function Page1() {
     return (
             <section data-anchor="Page 1" className="pg-page" style={pageSection}>
                 여긴 page1
+                뭐라도 하자
+                <div>
+                    브라우저 화면 사이즈
+                    x : {window.innerWidth},
+                    y : {window.innerHeight}
+                </div>
             </section>
         )
 }
