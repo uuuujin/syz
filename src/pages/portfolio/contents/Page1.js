8da39380271c4b7e2f4579744fc5841784e3cd63
yjshin2 <yjshin2@enliple.com>
2022-11-03 16:29:07 +0900

    portfolio 4개 페이지 임시 commit; -> 다시 commit;

--- src/pages/portfolio/contents/Page1.js
+++ src/pages/portfolio/contents/Page1.js
@@ -1,32 +1,16 @@
 import React from "react";
 import './Page.scss';
 //import { useState } from "react";
-import {useEffect} from "react";
+//import {useEffect} from "react";
 //import ReactDOM from "react-dom";
 
 export default function Page1() {
-
-    const ResizedComponent = () => {
-        const handleResize = () => {
-            console.log(`브라우저 화면 사이즈 : ${window.innerWidth}, y : ${window.innerHeight}`);
-        }
-
-         useEffect(( ) => {
-            window.addEventListener('resize', handleResize);
-            return () => {  //cleanup
-                window.removeEventListener('resize', handleResize);
-            }
-        }, []);
-    }
-
 
     return (
             <section data-anchor="Page 1" className="pg-page">
                 page1
                 <div>
-                    브라우저 화면 사이즈
-                    x : {window.innerWidth},
-                    y : {window.innerHeight}
+                    here is...page 1
                 </div>
             </section>
         )
